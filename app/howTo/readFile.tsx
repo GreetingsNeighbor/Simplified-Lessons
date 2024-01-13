aadf89b479bb3f8b481b43f4264062e094ad837b
Tu Nguyen <tu@tunguyen.com>
2024-01-12 22:22:52 -0500

    added read and write file functions for future ref

--- app/howTo/readFile.tsx
+++ app/howTo/readFile.tsx
@@ -1,16 +1,34 @@
 import { promises as fs } from "fs";
-//add descripton for the readFile function
 
 /**
  * 
- * @param relativePath - the path to the file relative to the root of the project; e.g. /src/components/Calendar.tsx
+ * @param relativePath - the path to the file relative to the root of the project; e.g. /app/data/Calendar.tsx
  * @returns 
  */
 export const readFile = async (relativePath: string) => {
     try {
         const data = await fs.readFile(process.cwd + relativePath, "utf-8");
-        console.log(data);
+        //if you want the data in json
+        //const dattJson=JSON.parse(data);
+        return data;
     } catch (error) {
         console.log(error);
+        return null;
+    }
+}
+
+/**
+ * 
+ * @param relativePath - the path to the file relative to the root of the project; e.g. /app/data/Calendar.tsx
+ * @param data - the data to be written to the file
+ * @returns 
+ */
+export const writeFile = async (relativePath: string, data: string) => {
+    try {
+        await fs.writeFile(process.cwd + relativePath, data);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
     }
 }
